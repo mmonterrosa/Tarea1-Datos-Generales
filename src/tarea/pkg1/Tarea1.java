4b845e17777cdc506b8615c287ba0eb440755cb4
MMont <MMont@LAPTOP-KCJJ3911>
2018-03-18 22:33:12 -0600

    New Commit

---
+++ src/tarea/pkg1/Tarea1.java
@@ -0,0 +1,18 @@
+
+package tarea.pkg1;
+
+/**
+ *
+ * @author MMont
+ */
+public class Tarea1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nombre: Marco Aurelio\n");
+        System.out.println("Apellidos: Monterrosa Soto\n");
+        System.out.println("Edad: 20 Años\n");
+        System.out.println("Estatura: 1.70 M.\n");
+        System.out.println("Sexo: Masculino \n");
+    }
+    
+}
